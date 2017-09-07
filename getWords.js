697927e92e99a37d819f07f04b93f7fd9b67bff5
absentees <scottblissett@gmail.com>
2017-09-07 23:07:35 +1000

    ahhh

--- getWords.js
+++ getWords.js
@@ -1,5 +1,6 @@
 #!/usr/bin/env node
 
+require('dotenv').config();
 var Xray = require('x-ray');
 var x = Xray();
 var Hashids = require('hashids');
@@ -7,8 +8,15 @@ var hashids = new Hashids('mrpoopybutthole', 10, 'abcdefghijklmnopqrstuvwxyz1234
 var moment = require('moment');
 var async = require('async');
 var json2csv = require('json2csv');
+var Airtable = require('airtable');
+Airtable.configure({
+	endpointUrl: 'https://api.airtable.com',
+	apiKey: process.env.AIRTABLE_API_KEY
+});
+var base = Airtable.base(process.env.AIRTABLE_BASE_ID);
 
 function getAirtableRecords(callback) {
+	console.log("Get airtable records.");
 	var records = [];
 
 	base('Domains').select({
@@ -28,14 +36,15 @@ function getAirtableRecords(callback) {
 		fetchNextPage();
 	}, function done(err) {
 		if (err) {
-			return callback(`Something went wrong getting records: ${err}`));
+			return callback(`Something went wrong getting records: ${err}`);
 		}
 
 		return callback(null, records);
 	});
 }
 
-function scrapeDictionary(callback) {
+function scrapeDictionary(records, callback) {
+	console.log("Scrape dictionary.");
 	var domains = [];
 
 	x('http://www.urbandictionary.com/', '.def-panel', [{
@@ -57,14 +66,29 @@ function scrapeDictionary(callback) {
 				domains[i].available = 'is taken';
 			}
 
-			callback(null, domains);
+			callback(null, records, domains);
 		});
 }
 
-function writeToFile(words, callback) {
+function mergeRecords(records, domains, callback) {
+	records.forEach(function(record) {
+		// Find matching domain
+		domains.forEach(function(domain){
+			if(record.title == domain.title){
+				console.log("Merging record: " + record.title);
+				record.word = domain.word;
+			}
+		});
+	});
+
+	callback(null, records);
+}
+
+function writeToFile(records, callback) {
 	try {
+		console.log("Write records to file.");
 		var result = json2csv({
-			data: words
+			data: records
 		});
 
 		require('fs').writeFile('./file.csv', result, function (err) {
@@ -75,9 +99,27 @@ function writeToFile(words, callback) {
 	}
 }
 
+function updateAirtableRecords(records, callback) {
+	async.each(records, function(record, eachCallback){
+		base('Domains').update(record.recordId, {
+			"word": record.word
+		}, function(err, record) {
+				if (err) { return eachCallback(err); }
+				eachCallback();
+		});
+	}, function(err) {
+		if (err) {
+			return callback(err);
+		}
+		callback(null, records);
+	});
+}
+
 async.waterfall([
+	getAirtableRecords,
 	scrapeDictionary,
-	writeToFile
+	mergeRecords,
+	updateAirtableRecords
 ], function (err, words) {
 	if (err) {
 		console.log(`Something went wrong: ${err}`);
